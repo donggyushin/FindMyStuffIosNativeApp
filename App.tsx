2aa82da334190c9d4b20a86dd5768f11f3e5694f
shin donggyu <donggyu9410@gmail.com>
2020-09-20 22:18:47 +0900

    [UPDATE] 로그인 되지 않은 화면으로 전환

--- App.tsx
+++ App.tsx
@@ -10,22 +10,18 @@
 
 import 'react-native-gesture-handler';
 
-import { Provider, useSelector } from 'react-redux'
-import store, { RootReducerType } from './src/store/Store'
+import {Provider, useSelector} from 'react-redux';
+import store, {RootReducerType} from './src/store/Store';
 
-import { NavigationContainer } from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
 import PrivateNavigation from './src/navigations/PrivateNavigation';
 import PublicNavigation from './src/navigations/PublicNavigation';
 import React from 'react';
-import {
-  StatusBar,
-} from 'react-native';
+import {StatusBar} from 'react-native';
 
-declare const global: { HermesInternal: null | {} };
+declare const global: {HermesInternal: null | {}};
 
 const App = () => {
-
-
   return (
     <Provider store={store}>
       <StatusBar barStyle="dark-content" />
@@ -35,10 +31,14 @@ const App = () => {
 };
 
 const Navigation = () => {
-  const userReducer = useSelector((state: RootReducerType) => state.UserReducer)
-  return (<NavigationContainer>
-    {!userReducer.token ? <PrivateNavigation /> : <PublicNavigation />}
-  </NavigationContainer>)
-}
+  const userReducer = useSelector(
+    (state: RootReducerType) => state.UserReducer,
+  );
+  return (
+    <NavigationContainer>
+      {userReducer.token ? <PrivateNavigation /> : <PublicNavigation />}
+    </NavigationContainer>
+  );
+};
 
 export default App;
